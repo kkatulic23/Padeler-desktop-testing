95f79e7ee0feb2a1e6164032c8eaa278b62591f3
fgrgac23 <fgrgac23@foi.hr>
2026-04-13 00:34:37 +0200

    Kreiran prvi testni scenarij za FZ04.

---
+++ AcceptanceTests/AcceptanceTests/Features/FZ04_Interaction.feature
@@ -0,0 +1,9 @@
+﻿Feature: FZ04_Interaction
+
+Background: 
+	Given I am logged into the application
+	And I am on the interaction screen
+
+Scenario: Available player profiles are displayed
+	When the interaction screen is opened
+	Then player cards should be displayed
--- AcceptanceTests/AcceptanceTests/obj/Debug/net48/AcceptanceTests.AssemblyInfo.cs
+++ AcceptanceTests/AcceptanceTests/obj/Debug/net48/AcceptanceTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AcceptanceTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70c231fd60ddecfe8abfba038d86935454c5f6fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed08d6675a4ac8cd56d30776434c752892abe214")]
 [assembly: System.Reflection.AssemblyProductAttribute("AcceptanceTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AcceptanceTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
