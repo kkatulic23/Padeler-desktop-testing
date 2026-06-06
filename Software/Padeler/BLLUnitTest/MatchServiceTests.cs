0a48dcb20e30ce44189429511160a1232724c591
fgrgac23 <fgrgac23@foi.hr>
2026-06-06 20:52:00 +0200

    Kreiran jedinični test LikeAsync_GivenMatchedSwipe_ReturnsTrue() koji provjerava uspješan LIKE koji rezultira MATCH-om.

--- Software/Padeler/BLLUnitTest/MatchServiceTests.cs
+++ Software/Padeler/BLLUnitTest/MatchServiceTests.cs
@@ -34,5 +34,27 @@ namespace BLLUnitTests
             // Assert
             Assert.False(result);
         }
+
+        [Fact]
+        public async Task LikeAsync_GivenMatchedSwipe_ReturnsTrue()
+        {
+            // Arrange
+            var swipeRepository = A.Fake<ISwipeRepository>();
+            var matchRepository = A.Fake<IMatchRepository>();
+
+            A.CallTo(() => swipeRepository.SwipeAsync(1, 2, "LIKE")).Returns(Task.FromResult(new SwipeResponse
+            {
+                Success = true,
+                Matched = true
+            }));
+
+            var service = new MatchService(swipeRepository, matchRepository);
+
+            // Act
+            var result = await service.LikeAsync(1, 2);
+
+            // Assert
+            Assert.True(result);
+        }
     }
 }
