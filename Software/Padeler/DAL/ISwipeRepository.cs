4e9659a86d108dad2c76b51870c3946fd85d7ba5
fgrgac23 <fgrgac23@foi.hr>
2026-06-06 13:38:19 +0200

    Dodana dva nova sučelja, ISwipeRepository i IUsersRepository za refaktoriranje ovisnosti servisa u svrhu jediničnog testiranja.

---
+++ Software/Padeler/DAL/ISwipeRepository.cs
@@ -0,0 +1,14 @@
+﻿using EL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public interface ISwipeRepository //Filip Grgac
+    {
+        Task<SwipeResponse> SwipeAsync(int fromUserId, int toUserId, string response);
+    }
+}
---
+++ Software/Padeler/DAL/IUsersRepository.cs
@@ -0,0 +1,18 @@
+﻿using EL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public interface IUsersRepository
+    {
+        Task<UserDto> GetUserAsync(int userId);
+
+        Task<List<UserDto>> GetNearbyUsersCardAsync(int currentUserId, double lat, double lng, int radius, string gender, string level, string position, string frequency);
+
+        Task<UserImageDto> GetImageForCardAsync(int userId);
+    }
+}
