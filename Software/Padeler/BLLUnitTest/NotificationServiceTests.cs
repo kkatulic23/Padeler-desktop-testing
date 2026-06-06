80272b6d0f900feafa405ced247db52525fd8952
fgrgac23 <fgrgac23@foi.hr>
2026-06-07 00:24:40 +0200

    Kreiran jedinični test MarkLatestMatchAsReadAsync_GivenUnreadMatchNotification_MarksNotificationAsRead() koji provjerava da se nepročitana Match nofitikacija označi kao pročitana.

--- Software/Padeler/BLLUnitTest/NotificationServiceTests.cs
+++ Software/Padeler/BLLUnitTest/NotificationServiceTests.cs
@@ -89,5 +89,31 @@ namespace BLLUnitTests
             // Assert
             A.CallTo(() => presenter.Show(A<Notification>._, A<Action>._)).MustNotHaveHappened();
         }
+
+        [Fact]
+        public async Task MarkLatestMatchAsReadAsync_GivenUnreadMatchNotification_MarksNotificationAsRead()
+        {
+            // Arrange
+            var notificaitonRepository = A.Fake<INotificationRepository>();
+            var presenter = A.Fake<INotificationPresenter>();
+
+            A.CallTo(() => notificaitonRepository.GetNotificationsAsync(1)).Returns(Task.FromResult(new List<Notification>
+            {
+                new Notification
+                {
+                    NotificationId = 5,
+                    Type = "MATCH",
+                    IsRead = false
+                }
+            }));
+
+            var service = new NotificationService(notificaitonRepository, presenter);
+
+            // Act
+            await service.MarkLatestMatchAsReadAsync(1);
+
+            // Assert
+            A.CallTo(() => notificaitonRepository.MarkAsReadAsync(5)).MustHaveHappenedOnceExactly();
+        }
     }
 }
