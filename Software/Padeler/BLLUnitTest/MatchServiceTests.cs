49a50b562adfe9c510ffa9a6a3c4090a10e2d838
fgrgac23 <fgrgac23@foi.hr>
2026-06-06 20:48:48 +0200

    Kreiran jedinični test LikeAsync_GivenMatchedSwipe_ReturnsFalse() koji provjerava postoji li uspješan LIKE ali nije MATCH.

---
+++ Software/Padeler/BLLUnitTest/MatchServiceTests.cs
@@ -0,0 +1,38 @@
+﻿using BLL;
+using DAL;
+using EL;
+using FakeItEasy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BLLUnitTests
+{
+    public class MatchServiceTests
+    {
+        [Fact]
+        public async Task LikeAsync_GivenMatchedSwipe_ReturnsFalse()
+        {
+            // Arrange
+            var swipeRepository = A.Fake<ISwipeRepository>();
+            var matchRepository = A.Fake<IMatchRepository>();
+
+            A.CallTo(() => swipeRepository.SwipeAsync(1, 2, "LIKE")).Returns(Task.FromResult(new SwipeResponse
+            {
+                Success = true,
+                Matched = false
+            }));
+
+            var service = new MatchService(swipeRepository, matchRepository);
+
+            // Act
+            var result = await service.LikeAsync(1, 2);
+
+            // Assert
+            Assert.False(result);
+        }
+    }
+}
