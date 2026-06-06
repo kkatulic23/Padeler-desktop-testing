7d64d2a24e097d9ca86aa09d1b1b7ee6fd2315e5
fgrgac23 <fgrgac23@foi.hr>
2026-06-06 15:20:43 +0200

    Kreiran jedinični test GetUSersForCardAsync_GivenUserIsNotLoggedIn_ThrowsException() koji provjerava da se kartice ne mogu dohvatiti ako korisnik nije prijavljen.

---
+++ Software/Padeler/BLLUnitTest/UserServiceTests.cs
@@ -0,0 +1,34 @@
+﻿using BLL;
+using DAL;
+using FakeItEasy;
+using System;
+using Xunit;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLLUnitTests
+{
+    public class UserServiceTests
+    {
+        public UserServiceTests()
+        {
+            AuthContext.Clear();
+        }
+
+        [Fact]
+        public async Task GetUSersForCardAsync_GivenUserIsNotLoggedIn_ThrowsException()
+        {
+            //Arrange
+            var userRepository = A.Fake<IUsersRepository>();
+            var service = new UserService(userRepository);
+
+            //Act
+            Func<Task> act = async () => await service.GetUsersForCardAsync(10, "", "", "", "");
+
+            //Assert
+            await Assert.ThrowsAsync<Exception>(act);
+        }
+    }
+}
