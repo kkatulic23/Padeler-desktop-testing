9e4342b64ecb84099cdae9ab2a59b63ae17c400f
fgrgac23 <fgrgac23@foi.hr>
2026-06-09 19:25:52 +0200

    Kreiran integracijski test LikeAsync_GivenSuccessfulMatch_ReturnsTrue() za provjeru rada kreiranja matcha. Također, kreirane pomoćne metode CreateDefaultMatchService() i StubPost() za lakšu izradu integracijskih testova.

---
+++ Software/Padeler/IntegrationTests/MatchServiceIntegrationTests.cs
@@ -0,0 +1,58 @@
+﻿using BLL;
+using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
+using WireMock.Server;
+using Xunit;
+
+namespace IntegrationTests
+{
+    public class MatchServiceIntegrationTests : IDisposable
+    {
+        private readonly WireMockServer _server;
+
+        public MatchServiceIntegrationTests()
+        {
+            _server = WireMockServer.Start();
+        }
+
+        [Fact]
+        public async Task LikeAsync_GivenSuccessfulMatch_ReturnsTrue()
+        {
+            // Arrange
+            StubPost("/api/match/swipe.php", "{\"success\":true,\"matched\":true,\"message\":\"Match created\"}");
+            var service = CreateDefaultMatchService();
+
+            // Act
+            var result = await service.LikeAsync(1, 2);
+
+            // Assert
+            Assert.True(result);
+        }
+
+        private MatchService CreateDefaultMatchService()
+        {
+            var apiClient = new ApiClient(new Uri(_server.Url + "/"));
+            var swipeRepository = new MatchesRepository(apiClient);
+            var matchRepository = new MatchListRepository(apiClient);
+
+            return new MatchService(swipeRepository, matchRepository);
+        }
+
+        private void StubPost(string path, string body)
+        {
+            _server.Given(Request.Create().WithPath(path).UsingPost()).RespondWith(Response.Create().WithStatusCode(200).WithHeader("Content-Type", "application/json").WithBody(body));
+        }
+
+        public void Dispose()
+        {
+            _server.Stop();
+            _server.Dispose();
+        }
+    }
+}
