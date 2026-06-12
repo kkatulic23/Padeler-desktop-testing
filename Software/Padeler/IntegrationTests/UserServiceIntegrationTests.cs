9c6eeb5896a085bdb097fb2b347d6e974715745c
fgrgac23 <fgrgac23@foi.hr>
2026-06-12 20:08:25 +0200

    Kreiran integracijski test GetUsersForCardAsync_GivenLoggedUserWithoutLocation_ReturnsEmptyList() koji provjerava vraća li se prazna lista korisnika ako trenutni korisnik nema uključenu lokaicju. Također, kreirani dodatni stubovi za laku implementaciju integracijskih testova.

--- Software/Padeler/IntegrationTests/UserServiceIntegrationTests.cs
+++ Software/Padeler/IntegrationTests/UserServiceIntegrationTests.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
 using WireMock.Server;
 using Xunit;
 
@@ -34,12 +36,42 @@ namespace IntegrationTests
             Assert.Equal("The user is not logged in!", exception.Message);
         }
 
+        [Fact]
+        public async Task GetUsersForCardAsync_GivenLoggedUserWithoutLocation_ReturnsEmptyList()
+        {
+            // Arrange
+            StubLoggedUserWithoutLocation();
+            var service = CreateDefaultUserService();
+
+            // Act
+            var result = await service.GetUsersForCardAsync(10, "", "", "", "");
+
+            // Assert
+            Assert.Empty(result);
+        }
+
         private UserService CreateDefaultUserService()
         {
             var apiClient = new ApiClient(new Uri(_server.Url + "/"));
             var repository = new UsersRepository(apiClient);
 
             return new UserService(repository);
+        }
+
+        private void StubLoggedUserWithoutLocation()
+        {
+            StubGet("/api/users/get_user.php", "{\"success\":true,\"user\":{\"UserId\":1,\"Name\":\"Filip\",\"Surname\":\"Grgac\",\"DateOfBirth\":\"2004-11-24\",\"Latitude\":null,\"Longitude\":null}}");
+            StubUserImage(1, "{\"success\":false,\"image_base64\":\"\",\"mime_type\":\"\"}");
+        }
+
+        private void StubGet(string path, string body)
+        {
+            _server.Given(Request.Create().WithPath(path).UsingGet()).RespondWith(Response.Create().WithStatusCode(200).WithHeader("Content-Type", "application/json").WithBody(body));
+        }
+
+        private void StubUserImage(int userId, string body)
+        {
+            _server.Given(Request.Create().WithPath("/api/users/get_user_image.php").WithParam("user_id", userId.ToString()).UsingGet()).RespondWith(Response.Create().WithStatusCode(200).WithHeader("Content-Type", "application/json").WithBody(body));
         }
 
         public void Dispose()
