70e7624e09456d9e200f34be88aa3755096118eb
kkrsak23 <kkrsak23@student.foi.hr>
2026-06-12 19:27:31 +0200

    Dodan IAuthRepository i jedinični testovi za AuthService.

---
+++ Software/Padeler/DAL/IAuthRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EL;
+
+namespace DAL
+{
+    public interface IAuthRepository
+    {
+        Task<int> LoginAsync(string username, string password);
+        Task<int> RegisterAsync(RegisterRequestDto dto);
+    }
+}
