83278fe21b961a5866ed092e7d56b6c96cf160ef
kkrsak23 <kkrsak23@student.foi.hr>
2026-06-13 22:58:12 +0200

    Dodan TDD ciklus za jako slabu lozinku.

---
+++ Software/Padeler/BLL/PasswordStrengthService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class PasswordStrengthService
+    {
+        public int CalculateScore(string password)
+        {
+            if (string.IsNullOrEmpty(password))
+            {
+                return 0;
+            }
+            return 0;
+        }
+
+        public string GetStrengthLabel(string password)
+        {
+            int score = CalculateScore(password);
+
+            if (score <= 1)
+            {
+                return "Very weak";
+            }
+            return "Very weak";
+        }
+    }
+}
---
+++ Software/Padeler/BLLUnitTest/PasswordStrengthServiceTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using BLL;
+
+namespace BLLUnitTests
+{
+    public class PasswordStrengthServiceTests
+    {
+        [Fact]
+        public void CalculateScore_GivenEmptyPassword_ReturnsZero()
+        {
+            // Arrange
+            var service = new PasswordStrengthService();
+
+            // Act
+            var result = service.CalculateScore("");
+
+            // Assert
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void GetStrengthLabel_GivenEmptyPassword_ReturnsVeryWeak()
+        {
+            // Arrange
+            var service = new PasswordStrengthService();
+
+            // Act
+            var result = service.GetStrengthLabel("");
+
+            // Assert
+            Assert.Equal("Very weak", result);
+        }
+    }
+}
